6da1caa64a288df5fe79f1253718fdc5b6b71990
tesstryan <tesstryan@gmail.com>
2014-02-23 14:59:06 -0600

    Updated - Current Temp now loading

--- app/models/characteristic.rb
+++ app/models/characteristic.rb
@@ -1,5 +1,21 @@
 class Characteristic < ActiveRecord::Base
 
+require 'open-uri'
+require 'json'
+
+
+	def get_current_temperature(city, state)
+	  forecast_url = "http://api.openweathermap.org/data/2.5/weather?q=#{city},#{state}"
+	  string_data = open(URI.encode(forecast_url)).read
+	  data = JSON.parse(string_data)
+
+	  temp = data["main"]["temp"]
+	  fahrenheit = (temp - 273.15)* 1.8000 + 32.00
+	  current_temp_degree = fahrenheit.to_i
+
+	 return current_temp_degree
+
+	end
 
 
 end
